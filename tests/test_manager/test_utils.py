12468d95e2e77c28a6d9a68d26735ccf129d9ebb
angrybayblade <vptl185@gmail.com>
2023-12-11 12:41:17 +0530

    fix: linters

--- tests/test_manager/test_utils.py
+++ tests/test_manager/test_utils.py
@@ -26,6 +26,7 @@ import pytest
 
 from aea.manager.utils import run_in_venv
 
+
 pytestmark = pytest.mark.skip(
     reason="Unknown issue: https://github.com/valory-xyz/open-aea/actions/runs/7162860465/job/19500538744?pr=698"
 )
