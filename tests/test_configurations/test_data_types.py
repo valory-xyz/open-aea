7963c9e5747cec909b7d6fbd72c51901064e1d1e
Adamantios <adamantioszaras@gmail.com>
2026-02-25 14:27:55 +0200

    chore: bump tomte

--- tests/test_configurations/test_data_types.py
+++ tests/test_configurations/test_data_types.py
@@ -165,19 +165,19 @@ class TestDependency:
     def test_parse_from_string(self) -> None:
         """Test from_string method."""
 
-        string = "tomte==0.4.0"
+        string = "tomte==0.6.0"
         dep = Dependency.from_string(string=string)
         assert dep.name == "tomte"
-        assert str(dep.version) == "==0.4.0"
+        assert str(dep.version) == "==0.6.0"
         assert dep.to_pip_string() == string
 
     def test_parse_from_string_wth_extras(self) -> None:
         """Test from_string method."""
 
-        string = "tomte[tox,tests]==0.4.0"
+        string = "tomte[tox,tests]==0.6.0"
         dep = Dependency.from_string(string=string)
         assert dep.name == "tomte"
-        assert str(dep.version) == "==0.4.0"
+        assert str(dep.version) == "==0.6.0"
         assert dep.extras == ["tox", "tests"]
         assert dep.to_pip_string() == string
 
@@ -194,19 +194,19 @@ class TestDependency:
     def test_parse_from_pipfile_specifier(self) -> None:
         """Test from_pipfile_specifier method"""
 
-        string = 'tomte = "==0.4.0"'
+        string = 'tomte = "==0.6.0"'
         dep = Dependency.from_pipfile_string(string)
         assert dep.name == "tomte"
-        assert str(dep.version) == "==0.4.0"
+        assert str(dep.version) == "==0.6.0"
         assert dep.to_pipfile_string() == string
 
     def test_parse_from_pipfile_specifier_with_extras(self) -> None:
         """Test from_pipfile_specifier method"""
 
-        string = 'tomte = {version = "==0.4.0", extras = ["tox", "tests"]}'
+        string = 'tomte = {version = "==0.6.0", extras = ["tox", "tests"]}'
         dep = Dependency.from_pipfile_string(string)
         assert dep.name == "tomte"
-        assert str(dep.version) == "==0.4.0"
+        assert str(dep.version) == "==0.6.0"
         assert dep.extras == ["tox", "tests"]
         assert dep.to_pipfile_string() == string
 
