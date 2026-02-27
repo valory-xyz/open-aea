251ff0dd6db63dd9be1f30b102c86221f31dd52f
OjusWiZard <ojuswimail@gmail.com>
2026-02-27 20:43:29 +0530

    Chore: Release `2.1.0rc2`
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- plugins/aea-ledger-solana/setup.py
+++ plugins/aea-ledger-solana/setup.py
@@ -24,7 +24,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="open-aea-ledger-solana",
-    version="2.1.0rc1",
+    version="2.1.0rc2",
     author="dassy23",
     license="Apache-2.0",
     description="Python package wrapping the public and private key cryptography and ledger api of solana.",
