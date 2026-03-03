ea76f507a495598be48fe7081e1de6087c61a4e7
OjusWiZard <ojuswimail@gmail.com>
2026-03-03 16:15:01 +0530

    chore: bump `open-aea==2.1.0rc6`
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- plugins/aea-ledger-solana/setup.py
+++ plugins/aea-ledger-solana/setup.py
@@ -24,7 +24,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="open-aea-ledger-solana",
-    version="2.1.0rc5",
+    version="2.1.0rc6",
     author="dassy23",
     license="Apache-2.0",
     description="Python package wrapping the public and private key cryptography and ledger api of solana.",
